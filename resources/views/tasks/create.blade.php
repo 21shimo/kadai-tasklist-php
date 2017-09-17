2b3e97d3834652a7571f411b93397380595e12e4
yusukeshimotani <21shimo@gmail.com>
2017-09-17 08:05:25 +0000

    bootstrap design

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -3,15 +3,25 @@
 @section('content')
    <h1>新規タスク作成ページ</h1>
    
-   {!! Form::model($task, ['route' => 'tasks.store']) !!}
-       {!! Form::label('status', 'ステータス:') !!}
-       {!! Form::text('status') !!}
+   <div class="row">
+       <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-lg-offset-3 col-lg-6">
+           
+           {!! Form::model($task, ['route' => 'tasks.store']) !!}
 
-       {!! Form::label('content', 'タスク:') !!}
-       {!! Form::text('content') !!}
+               <div class="form-group">
+                   {!! Form::label('status', 'ステータス:') !!}
+                   {!! Form::text('status', null, ['class' => 'form-control']) !!}
+               </div>
 
-       {!! Form::submit('作成') !!}
-       
-   {!! Form::close() !!}
+               <div class="form-group">
+                   {!! Form::label('content', 'タスク:') !!}
+                   {!! Form::text('content', null, ['class' => 'form-control']) !!}
+               </div>
+
+               {!! Form::submit('作成', ['class' => 'btn btn-default']) !!}
+
+           {!! Form::close() !!}
+       </div>
+   </div>
    
 @endsection
