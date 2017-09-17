6fdffb796f1539333d907b3e5a8b39a0ed36f9fb
yusukeshimotani <21shimo@gmail.com>
2017-09-17 06:29:47 +0000

    column add

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -4,9 +4,12 @@
    <h1>新規タスク作成ページ</h1>
    
    {!! Form::model($task, ['route' => 'tasks.store']) !!}
+       {!! Form::label('status', 'ステータス:') !!}
+       {!! Form::text('status') !!}
+
        {!! Form::label('content', 'タスク:') !!}
        {!! Form::text('content') !!}
-       
+
        {!! Form::submit('作成') !!}
        
    {!! Form::close() !!}
